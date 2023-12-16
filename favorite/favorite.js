d663f52292aa638656b5dca0a94feb066b2fc4a3
Din <Dinz5005@gmail.com>
2023-12-16 21:29:48 +0200

    newHeaderStyle

--- favorite/favorite.js
+++ favorite/favorite.js
@@ -1,9 +1,9 @@
-const storedPic = localStorage.getItem("favorite");
+const storedPic = localStorage.getItem('favorite');
 const picId = JSON.parse(storedPic);
-const query = localStorage.getItem("query");
-const table = document.querySelector("table");
-const article = document.querySelector("article");
-const main = document.querySelector("main");
+const query = localStorage.getItem('query');
+const table = document.querySelector('table');
+const article = document.querySelector('article');
+const main = document.querySelector('main');
 
 const getData = async () => {
   try {
@@ -11,29 +11,29 @@ const getData = async () => {
     const data = await res.json();
     const favoriteData = filteredData(data, picId);
     for (const data of favoriteData) {
-      const tr = document.createElement("tr");
+      const tr = document.createElement('tr');
       tr.innerHTML = `
       <tbody>
       <td>${data.show.name}</td>
       <td>${data.show.type}</td>
       <td>${data.show.language}</td>
-      <td>${data.show.rating.average ? data.show.rating.average : "No data available"}</td>
+      <td>${data.show.rating.average ? data.show.rating.average : 'No data available'}</td>
       <td><a href="${data.show.url}">Details</a></td>
       </tbody>`;
       table.append(tr);
     }
   } catch (error) {
-    article.remove()
-    const Error = document.createElement("div");
-    Error.innerHTML = 'No TV Shows Available'
-    Error.className = 'text-center'
-    Error.style.fontFamily = 'cursive'
+    article.remove();
+    const Error = document.createElement('div');
+    Error.innerHTML = 'No TV Shows Available';
+    Error.className = 'text-center';
+    Error.style.fontFamily = 'cursive';
     main.append(Error);
   }
 };
 
 const filteredData = (api, imgList) => {
-  return api.filter(obj => {
+  return api.filter((obj) => {
     if (obj.show.image) {
       for (const img of imgList) {
         if (obj.show.image.medium === img) {
--- main/app.js
+++ main/app.js
@@ -1,54 +1,54 @@
-import { MakeObj, container, resetShows } from "./class.js";
+import {MakeObj, container, resetShows} from './class.js';
 
-const form = document.querySelector("form");
-const reset = document.querySelector("#reset");
-const submit = document.querySelector(".submit");
-const favorite = document.querySelector(".favorite");
+const form = document.querySelector('form');
+const reset = document.querySelector('#reset');
+const submit = document.querySelector('.submit');
+const favorite = document.querySelector('.favorite');
 const bodyElement = document.body;
 let flag = 0;
 
 window.addEventListener('resize', handleResize);
 
-form.addEventListener("submit", async (e) => {
+form.addEventListener('submit', async (e) => {
   e.preventDefault();
-  localStorage.clear()
+  localStorage.clear();
   let searchTerm = form.elements.query.value;
-  if (searchTerm !== "") {
+  if (searchTerm !== '') {
     try {
       const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchTerm}`);
-      if (res.data.length == 0) throw new Error('Empty list')
-      bodyBG()
+      if (res.data.length == 0) throw new Error('Empty list');
+      bodyBG();
       makeTvShow(res.data);
       submit.disabled = true;
-      localStorage.setItem("query", searchTerm);
-      container.style.display = 'grid'
-      window.location.href = "#shows";
+      localStorage.setItem('query', searchTerm);
+      container.style.display = 'grid';
+      window.location.href = '#shows';
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-    searchTerm = "";
+    searchTerm = '';
   }
 });
 
-reset.addEventListener("click", () => {
+reset.addEventListener('click', () => {
   bodyElement.style.backgroundImage = 'url("./assets/TvShow.jpg")';
-  container.style.display = 'none'
-  favorite.innerHTML = '0'
-  removeAllChildren(container)
-  container.style.display = 'none'
-  form.elements.query.value = "";
+  container.style.display = 'none';
+  favorite.innerHTML = '0';
+  removeAllChildren(container);
+  container.style.display = 'none';
+  form.elements.query.value = '';
   submit.disabled = false;
   flag = 0;
-  localStorage.clear()
-  resetShows()
+  localStorage.clear();
+  resetShows();
 });
 
-const bodyBG = () => bodyElement.style.backgroundImage = window.innerWidth > 990 ? 'url("./assets/frame.jpg")' : 'url("./assets/SmallBG.jpg")'
+const bodyBG = () => (bodyElement.style.backgroundImage = window.innerWidth > 990 ? 'url("./assets/frame.jpg")' : 'url("./assets/SmallBG.jpg")');
 
 function handleResize() {
   const bodyStyle = window.getComputedStyle(bodyElement);
   const backgroundImage = bodyStyle.getPropertyValue('background-image');
-  !backgroundImage.includes('TvShow') && bodyBG()
+  !backgroundImage.includes('TvShow') && bodyBG();
 }
 
 const makeTvShow = (data) => {
@@ -67,4 +67,3 @@ function removeAllChildren(element) {
     element.removeChild(element.firstChild);
   }
 }
-
